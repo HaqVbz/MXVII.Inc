44c65a7856a0324a1473c4270a9a916a9483093a
Haqvibes <61591069+HaqVbz@users.noreply.github.com>
2024-06-18 08:30:05 +0700

    Add files via upload

---
+++ src/components/Header/nav/Footer/index.jsx
@@ -0,0 +1,12 @@
+import styles from './style.module.scss';
+
+export default function index() {
+  return (
+    <div className={styles.footer}>
+        <a>Awwwards</a>
+        <a>Instagram</a>
+        <a>Twitter</a>
+        <a>Behance</a>
+    </div>
+  )
+}
